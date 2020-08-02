62e931764a019307ec925f58d1f4102666eea0ba
shashank <mshashank@gmail.com>
2020-08-02 10:46:44 -0400

    sample git static page - wip

---
+++ memory-game/precache-manifest.a64399e808e83345d5abcdf280f4bef3.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e12449f24eacf77ce4f99a7be64fc36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77adf07f49e655029d3d",
+    "url": "./static/js/2.c453fb5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c453fb5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51fcd043156b77893358",
+    "url": "./static/js/main.4dc50e41.chunk.js"
+  },
+  {
+    "revision": "0a14084593c902ea4944",
+    "url": "./static/js/runtime-main.a4465c83.js"
+  },
+  {
+    "revision": "ee9a8073eb3f04a2ad6978ea1413aa94",
+    "url": "./static/media/bee.ee9a8073.png"
+  },
+  {
+    "revision": "33d780bd53849a749ae21eb1dd4a6019",
+    "url": "./static/media/card.33d780bd.png"
+  },
+  {
+    "revision": "b7262f536c66b0b90403ee23937d86ad",
+    "url": "./static/media/cheetah.b7262f53.png"
+  },
+  {
+    "revision": "0e41b832c949ead159b7308113df7d12",
+    "url": "./static/media/croc.0e41b832.png"
+  },
+  {
+    "revision": "5b9a2f7720368dea82bdfd1bb42aad1e",
+    "url": "./static/media/deer.5b9a2f77.png"
+  },
+  {
+    "revision": "bfe4271b48fabbf7a4e3b08f7104a85c",
+    "url": "./static/media/dog.bfe4271b.png"
+  },
+  {
+    "revision": "40b9276b77e04a6a16079f3683cbb111",
+    "url": "./static/media/dolphin.40b9276b.png"
+  },
+  {
+    "revision": "cb52ba95541f854a2f6aaeed6d03e5fd",
+    "url": "./static/media/elephant.cb52ba95.png"
+  },
+  {
+    "revision": "65532169059f084c8b079bb989002c4b",
+    "url": "./static/media/fox.65532169.png"
+  },
+  {
+    "revision": "6769b4c1a9bb417838dad0ca1137233e",
+    "url": "./static/media/giraffe.6769b4c1.png"
+  },
+  {
+    "revision": "36602689056908f62540c8c1a604aac4",
+    "url": "./static/media/kangaroo.36602689.png"
+  },
+  {
+    "revision": "0907daa26ffa44da850596d96fe71368",
+    "url": "./static/media/koala.0907daa2.png"
+  },
+  {
+    "revision": "b7991b0a5cef06621d63df258ce37ddb",
+    "url": "./static/media/lemur.b7991b0a.png"
+  },
+  {
+    "revision": "9c22ed4aa0b6d21bb9aa737358dc8a3d",
+    "url": "./static/media/monkey.9c22ed4a.png"
+  },
+  {
+    "revision": "b2dc5cca7ba5edf316ad24eb91b5eb60",
+    "url": "./static/media/ostrich.b2dc5cca.png"
+  },
+  {
+    "revision": "6f8445084834b8b4384c9819e9bfe958",
+    "url": "./static/media/panda.6f844508.png"
+  },
+  {
+    "revision": "bf9198bf71a3b70c4b52ce032bfc29a5",
+    "url": "./static/media/rabbit.bf9198bf.png"
+  },
+  {
+    "revision": "8bcd07a1a4cb424333a92d637ae4971b",
+    "url": "./static/media/rhino.8bcd07a1.png"
+  },
+  {
+    "revision": "05b18c56be7f420b3382aec59307e8d0",
+    "url": "./static/media/shark.05b18c56.png"
+  },
+  {
+    "revision": "937ee6c02fe46a91e96c66e1d6479c54",
+    "url": "./static/media/sheep.937ee6c0.png"
+  },
+  {
+    "revision": "bad46740ceab217667e807657ba70ab4",
+    "url": "./static/media/snake.bad46740.png"
+  },
+  {
+    "revision": "2218c52fa63cff6655fc873019de57ee",
+    "url": "./static/media/tiger.2218c52f.png"
+  },
+  {
+    "revision": "8c4e7aaa15bee1ca0eefba0b53e9626e",
+    "url": "./static/media/tortoise.8c4e7aaa.png"
+  },
+  {
+    "revision": "9df862bc469b6bae867791818331204f",
+    "url": "./static/media/tucan.9df862bc.png"
+  },
+  {
+    "revision": "26d43ecd496515ab7d427ad012f0112f",
+    "url": "./static/media/zebra.26d43ecd.png"
+  }
+]);
